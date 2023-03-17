7dfd1a94d2ad95caf5c4cd0860f18fb0480b12b5
pramodyasahan <sahandil1013@gmail.com>
2023-03-18 00:03:12 +0530

    feat: SendMyName

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,15 @@ import express, {Request, Response} from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World!');
+    const myName: string = SendMyName()
+    console.log(`My Name has sent as the response`)
+    res.send(`Hello, World! ${myName}`);
 });
+
+export function SendMyName() {
+    return `I am Sahan`
+
+}
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
