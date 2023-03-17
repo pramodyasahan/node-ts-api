e782302f803f13bfd34fe82b7e5da50cc8d7315d
pramodyasahan <sahandil1013@gmail.com>
2023-03-18 00:20:20 +0530

    feat: add reset and number methods

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,25 @@ import express, {Request, Response} from 'express';
 
 const app = express();
 
+let number: number = 0
+
 app.get('/', (req: Request, res: Response) => {
     const myName: string = SendMyName()
-    console.log(`My Name has sent as the response`)
-    res.send(`Hello, World! ${myName}`);
+    let response: string = `Hello, World! ${myName},\n and the new number is ${number}`;
+    console.log(`My Name has sent as the response`, response)
+    res.send(response);
+});
+
+app.get('/reset', (req: Request, res: Response) => {
+    number = 0
+    console.log(`My Name has sent as the response`, `Number has been reset, and the new number is ${number}`)
+    let numberResetResponse: string = `Number has been reset, and the new number is ${number}`;
+    res.send(numberResetResponse);
 });
 
 export function SendMyName() {
+    number += 1;
     return `I am Sahan`
-
 }
 
 app.listen(3000, () => {
